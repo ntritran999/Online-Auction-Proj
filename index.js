b059d47b2c432494d6753a8b4335383e2cf5ff21
ntritran999 <ntritran999@gmail.com>
2026-01-05 11:58:47 +0700

    Fix missing relative times of dayjs in index.js

--- index.js
+++ index.js
@@ -57,13 +57,18 @@ dayjs.updateLocale('en', {
     relativeTime: {
         future: "%s nữa",
         past: "%s trước",
-        s: 'vài giây',
+        s: "vài giây",
+        ss: "%d giây",
         m: "1 phút",
         mm: "%d phút",
         h: "1 giờ",
         hh: "%d giờ",
         d: "1 ngày",
         dd: "%d ngày",
+        M: "1 tháng",
+        MM: "%d tháng",
+        y: "1 năm",
+        yy: "%d năm",
     }
 });
 dayjs.extend(utc);
